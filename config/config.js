e0b0684f1e306f3ad24bf49d339481f57fa37486
[Daniel Costella] <[dcostella93@gmail.com]>
2024-08-11 23:04:14 -0300

    Configuración de Sequelize y dotenv para la conexión a la base de datos

--- config/config.js
+++ config/config.js
@@ -1,22 +1,19 @@
-require('dotenv').config();
-
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASS:', process.env.DB_PASS);
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // Asegúrate de incluir el puerto si es necesario
   dialect: 'postgres',
   logging: false,
 });
 
 sequelize.authenticate()
   .then(() => {
-    console.log('Conexión establecida correctamente.');
+    console.log('Conexión a PostgreSQL establecida correctamente.');
   })
   .catch(err => {
     console.error('No se pudo conectar a la base de datos:', err);
   });
 
-module.exports = sequelize; // Exportar la instancia de Sequelize
+module.exports = sequelize;
